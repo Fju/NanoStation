57e40d938b302339d74b4da8e1d17f4dca588aee
Julien Mourer <getkey@users.noreply.github.com>
2017-03-04 19:49:20 +0100

    Fix the score counter
    Because it wasn't null-terminated, it was displaying what was in memory after the score (which was "KB" for some reason).

--- src/src.ino
+++ src/src.ino
@@ -1,10 +1,6 @@
 #include <VGAX.h>
 #include <avr/pgmspace.h>
 
-#define VGAX_HEIGHT         60
-#define VGAX_BWIDTH         30
-#define VGAX_WIDTH          120
-
 #define FNT_SYMBOLS_COUNT   36
 #define FNT_HEIGHT          5
 #define FNT_BYTE_LENGTH     4
@@ -93,31 +89,31 @@ byte buttons; // [ btn1 ] [ btn2 ] //2 bit
 void processInputs() {
   stick_pos = 0;
   buttons = 0;
-  
+
   int sx = map(analogRead(PIN_STICK_X), 178, 871, 0, 1000);
   int sy = map(analogRead(PIN_STICK_Y), 206, 860, 0, 1000);
   if (sx < 140) stick_pos |= 2;
   if (sx > 860) stick_pos |= 1;
   if (sy < 140) stick_pos |= 4;
-  if (sy > 860) stick_pos |= 8; 
-  
+  if (sy > 860) stick_pos |= 8;
+
   if (digitalRead(PIN_BUTTON_A) == HIGH) buttons |= 1;
   if (digitalRead(PIN_BUTTON_B) == HIGH) buttons |= 2;
   if (digitalRead(PIN_STICK_BUTTON) == HIGH) buttons |= 4;
 }
 
 
-byte getWidth(const char* text, boolean progmem) {   
+byte getWidth(const char* text, boolean progmem) {
   char *pstr = (char*)text;
   char c;
-  
+
   byte width = 0;
   if (progmem){
    while (c = pgm_read_byte(pstr++)) width += charWidth(c);
   } else {
     while (c = *pstr++) width += charWidth(c);
   }
-  
+
   return width;
 }
 
@@ -133,20 +129,20 @@ byte charWidth(char c) {
 
 
 byte drawCharacter(char c, byte x, byte y, byte colour) {
-  if (c == 32) return 3;    
-    
+  if (c == 32) return 3;
+
   if (c >= 48 && c < 65) c -= 48; //char code 48 to index 0 in fnt_data
   if (c >= 65 && c < 97) c -= 55; //char code 65 to index 10 in fnt_data
-      
+
   if (c >= 0 && c < FNT_SYMBOLS_COUNT) {
     unsigned long glyph = 0;
     byte bytelength = FNT_BYTE_LENGTH; //32 bit
     while (bytelength--) glyph |= (unsigned long)pgm_read_byte((byte*)fnt_data + c * FNT_BYTE_LENGTH + bytelength) << (8 * (FNT_BYTE_LENGTH - bytelength - 1));
-      
+
     byte cwidth = glyph & 0b111; //byte is unsigned
 
     glyph >>= 7;
-     
+
     byte gheight = FNT_HEIGHT, gwidth;
     while (gheight--) {
       gwidth = cwidth;
@@ -154,23 +150,23 @@ byte drawCharacter(char c, byte x, byte y, byte colour) {
         if (glyph >> (cwidth * gheight + gwidth) & 1) vga.putpixel(x + cwidth - gwidth - 1, y + FNT_HEIGHT - gheight - 1, colour);
       }
     }
-    
+
     return cwidth + 1;
   }
-  return 0; 
+  return 0;
 }
 
 void drawTextPROGMEM(const char* text, byte x, byte y, byte colour) {
-  char *pstr = (char*)text;  
-  byte dx = x;  
+  char *pstr = (char*)text;
+  byte dx = x;
   char c;
   while (c = pgm_read_byte(pstr++)) dx += drawCharacter(c, dx, y, colour);
 }
 
 void drawText(const char* text, byte x, byte y, byte colour) {
-  char *pstr = (char*)text;  
+  char *pstr = (char*)text;
   byte dx = x;
-  char c;  
+  char c;
   while (c = *pstr++) dx += drawCharacter(c, dx, y, colour);
 }
 
@@ -178,16 +174,16 @@ void reset() {
   vga.clear(COLOUR_BLACK);
   DRAW_BORDER;
   CLEAR_GAME;
-  
+
   score = 0;
   last_score = 255;
   vel_x = -1;
   vel_y = 0;
   length = 3;
-  
+
   food = false;
   spawn_timer = 0;
-  
+
   last_x[0] = GAME_WIDTH / 2;
   last_y[0] = GAME_HEIGHT / 2;
 }
@@ -196,20 +192,20 @@ void reset() {
 void setup() {
   randomSeed(analogRead(A0)); //generate random seed
   vga.begin();
-  vga.clear(COLOUR_BLUE);  
-  
+  vga.clear(COLOUR_BLUE);
+
   gameState = 0;
-  
+
   byte width_title = getWidth(str_title, true);
   byte width_instr = getWidth(str_instructions, true);
-  drawTextPROGMEM(str_title, (VGAX_WIDTH - width_title) / 2, 20, COLOUR_YELLOW);    
+  drawTextPROGMEM(str_title, (VGAX_WIDTH - width_title) / 2, 20, COLOUR_YELLOW);
   drawTextPROGMEM(str_instructions, (VGAX_WIDTH - width_instr) / 2, 40, COLOUR_WHITE);
 }
 
-void loop() {  
-  processInputs();  
- 
-  if (gameState == 1) {    
+void loop() {
+  processInputs();
+
+  if (gameState == 1) {
     if (stick_pos & 1 && vel_x != 1) {
       vel_x = -1;
       vel_y = 0;
@@ -223,31 +219,32 @@ void loop() {
       vel_x = 0;
       vel_y = -1;
     }
-    
+
     boolean gameover = false;
-    
-    if (last_x[0] + vel_x < 0 || last_x[0] + vel_x > GAME_WIDTH - 1 || last_y[0] + vel_y < 0 || last_y[0] + vel_y > GAME_HEIGHT - 1) {      
+
+    if (last_x[0] + vel_x < 0 || last_x[0] + vel_x > GAME_WIDTH - 1 || last_y[0] + vel_y < 0 || last_y[0] + vel_y > GAME_HEIGHT - 1) {
       gameover = true;
-    } else {      
+    } else {
       if (last_score != score) {
-        char scoreStr[] = {0, 0, 0};
+        char scoreStr[4];
+        scoreStr[3] = 0;
         char *scrptr = &scoreStr[2];
         byte s = score, sl = 3;
         while (sl--) {
           *scrptr = (s % 10) + 48;
           s /= 10;
           scrptr--;
-        }       
-        
+        }
+
         byte twidth = getWidth(scoreStr, false);
         vga.fillrect(VGAX_WIDTH - twidth - 3, 10, twidth, FNT_HEIGHT, COLOUR_BLACK);
-        
+
         drawText(scoreStr, VGAX_WIDTH - twidth - 3, 10, COLOUR_WHITE);
         drawTextPROGMEM(str_score, VGAX_WIDTH - 23, 3, COLOUR_YELLOW);
       }
-      
-      last_score = score;           
-            
+
+      last_score = score;
+
       byte l = length;
       while (l--) {
         if (l == length - 1) vga.putpixel(last_x[l] + GAME_BORDER, last_y[l]+ GAME_BORDER, COLOUR_BLACK);
@@ -256,12 +253,12 @@ void loop() {
           last_y[l] += vel_y;
         } else {
           last_x[l] = last_x[l - 1];
-          last_y[l] = last_y[l - 1];          
+          last_y[l] = last_y[l - 1];
         }
-        
+
         vga.putpixel(last_x[l] + GAME_BORDER, last_y[l] + GAME_BORDER, COLOUR_WHITE);
       }
-      
+
       byte cx = last_x[0], cy = last_y[0];
       l = length;
       while (l--) {
@@ -270,9 +267,9 @@ void loop() {
           break;
         }
       }
-            
-      if (cx == food_x && cy == food_y) {        
-        length++;        
+
+      if (cx == food_x && cy == food_y) {
+        length++;
         l = length;
         while (--l != 0) {
           last_x[l] = last_x[l - 1];
@@ -280,12 +277,12 @@ void loop() {
         }
         last_x[0] = cx;
         last_y[0] = cy;
-        
+
         food = false;
         spawn_timer = 0;
         score++;
       }
-      
+
       if (!food) {
         spawn_timer = ++spawn_timer % 30; //30 * 150ms
         if (spawn_timer == 0) {
@@ -293,7 +290,7 @@ void loop() {
           food_y = random(GAME_HEIGHT);
           food = true;
         }
-      } else vga.putpixel(food_x + GAME_BORDER, food_y + GAME_BORDER, COLOUR_BLUE);       
+      } else vga.putpixel(food_x + GAME_BORDER, food_y + GAME_BORDER, COLOUR_BLUE);
     }
 
     if (gameover) {
@@ -306,12 +303,12 @@ void loop() {
     else if (score >= 8 && score < 12) vga.delay(60);
     else if (score >= 12 && score < 16) vga.delay(40);
     else vga.delay(33);
-    
+
   } else { //Game Over    
     if (buttons) {
       reset();
       gameState = 1;
-    }  
+    }
     vga.delay(20);
-  }  
+  }
 }
