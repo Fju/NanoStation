0d958a2a7886d19d0258fff8aa9a31b10f266067
Julien Mourer <getkey@getkey.eu>
2018-05-03 20:37:23 +0200

    Make the platforms blink

--- src/up2/character.cpp
+++ src/up2/character.cpp
@@ -3,6 +3,8 @@
 #include "text.h"
 #include <VGAX.h>
 
+#define SENSIBILITY 200
+
 // the character is just 1 pixel which simplifies collisions
 Character::Character(signed char x_, signed char y_) :
 x(x_), y(y_), prev_x(x_), prev_y(y_), vel(0, 0) {
@@ -14,6 +16,14 @@ void Character::draw() {
 }
 
 void Character::update() {
+	int sx = analogRead(PIN_STICK_X);
+	int sy = analogRead(PIN_STICK_Y);
+
+	this->vel.x += sx - 512 >> 7;
+	if (sy < 512 - SENSIBILITY) this->vel.y -= 3;
+
+	if (this->vel.y < 6) ++this->vel.y;
+
 	this->prev_x = this->x;
 	this->prev_y = this->y;
 }
--- src/up2/up2.ino
+++ src/up2/up2.ino
@@ -7,8 +7,8 @@
 #include "platform.h"
 #include "character.h"
 
-#define SENSIBILITY 200
 #define PLATFORM_AMOUNT 4
+#define BLINK_INTERVAL 20
 
 Platform platforms[PLATFORM_AMOUNT] = {
 	Platform(30, 30, 20, 20),
@@ -19,36 +19,36 @@ Platform platforms[PLATFORM_AMOUNT] = {
 
 Character character = Character(0, 0);
 
-void draw_platforms() {
-	for (byte i = 0; i != PLATFORM_AMOUNT; ++i) {
-		platforms[i].draw(COLOR_BLACK);
+void draw_platforms(byte color) {
+	platforms[0].draw(COLOR_BLACK); // the first platform doesn't blink
+	for (byte i = 1; i != PLATFORM_AMOUNT; ++i) {
+		platforms[i].draw(color);
 	}
 }
+
+bool platform_color = 0;
+byte platform_blink_counter = 0;
 
 void setup() {
 	VGAX::begin();
 	VGAX::clear(COLOR_BLUE);
-	draw_platforms();
+	draw_platforms(platform_color);
 }
 
+
 void loop() {
-	int sx = analogRead(PIN_STICK_X);
-	int sy = analogRead(PIN_STICK_Y);
-
-	character.vel.x += sx - 512 >> 7;
-	if (sy < 512 - SENSIBILITY) character.vel.y -= 3;
-
-	if (character.vel.y < 6) ++character.vel.y;
-
+	if (platform_blink_counter == BLINK_INTERVAL) platform_color = !platform_color;
+	platform_blink_counter = (platform_blink_counter + 1) % BLINK_INTERVAL + 1;
 
 	// logging
 	// VGAX::fillrect(10, 10, 50, 30, COLOR_BLUE);
 	// drawInt(character.x, 10, 10, COLOR_YELLOW);
 	// drawInt(character.y, 10, 20, COLOR_YELLOW);
 
-	/* character.update(platforms, PLATFORM_AMOUNT); */
+	// updates
+	character.update();
 
-	/* draw_platforms(); */
+	draw_platforms(platform_color);
 	character.draw();
 
 	VGAX::delay(33);
